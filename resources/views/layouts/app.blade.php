9acc17d2d60ac4fca65925508ae650a4f84bc93f
iswanti <66273551+iswanti@users.noreply.github.com>
2026-06-02 12:26:56 +0700

    Update name app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Sipensa RGI') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo-rgi.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,25 +17,6 @@
         
         
     </head>
-    {{-- <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body> --}}
 
     <body class="font-sans antialiased bg-gray-50">
         {{-- <div x-data="{ sidebarOpen: true }" class="flex">
